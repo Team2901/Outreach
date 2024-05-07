6342575a453e2ca7e9ec0ddfaacb5be42dfdb053
NickJerden <jerdenn25556@student.cghsnc.org>
2024-05-07 16:02:49 -0400

    ddr clawbot fix and disagnostic

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Outreach/TeleOp/ClawbotDDRTeleop.java
@@ -0,0 +1,122 @@
+package org.firstinspires.ftc.teamcode.Outreach.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Outreach.Hardware.ClawbotHardware;
+import org.firstinspires.ftc.teamcode.Shared.Gamepad.DDRGamepad;
+import org.firstinspires.ftc.teamcode.Shared.Gamepad.ImprovedGamepad;
+
+public class ClawbotDDRTeleop extends OpMode {
+    public ImprovedGamepad gamepad;
+    public double voltage;
+    public boolean reversed = false;
+    public boolean reversedTurns = false;
+
+    public int speedBoostFactor = 2;
+    public ImprovedGamepad masterGamepad;
+    static DDRGamepad participantGamepad;
+    public ElapsedTime timer = new ElapsedTime();
+    public double rightPower = 0;
+    public double leftPower = 0;
+
+
+    public boolean gamepadOverride = false;
+
+    ClawbotHardware robot = new ClawbotHardware();
+    @Override
+    public void init() {
+        robot.init(hardwareMap);
+
+        gamepad = new ImprovedGamepad(this.gamepad1, this.timer, "GP1");
+        masterGamepad = new ImprovedGamepad(this.gamepad2, this.timer, "GP2");
+        robot.currentArmState = ClawbotHardware.ArmState.HIGH;
+    }
+
+    @Override
+    public void loop() {
+        armPositionUpdate();
+        double rightSpeed = 0;
+        double leftSpeed = 0;
+
+        if (participantGamepad.upArrow.getValue()) {
+            if (!reversed) {
+                telemetry.addData("upArrow", participantGamepad.upArrow);
+                leftSpeed = .5 * speedBoostFactor;
+                rightSpeed = .5 * speedBoostFactor;
+            } else {
+                leftSpeed = -.5 * speedBoostFactor;
+                rightSpeed = -.5 * speedBoostFactor;
+            }
+
+        } else if (participantGamepad.downArrow.getValue()) {
+            if (!reversed) {
+                leftSpeed = -.5 * speedBoostFactor;
+                rightSpeed = -.5 * speedBoostFactor;
+            } else {
+                leftSpeed = .5 * speedBoostFactor;
+                rightSpeed = .5 * speedBoostFactor;
+            }
+
+        }
+
+        if (participantGamepad.leftArrow.getValue()) {
+            if (!reversedTurns) {
+                if (rightSpeed == 0 && leftSpeed == 0) {
+                    leftSpeed = -.4;
+                    rightSpeed = .4;
+                } else if (leftSpeed > 0){
+                    rightSpeed = .65;
+                } else {
+                    rightSpeed = -.65;
+                }
+            } else {
+                if (rightSpeed == 0 && leftSpeed == 0) {
+                    leftSpeed = .4;
+                    rightSpeed = -.4;
+                } else if (leftSpeed > 0){
+                    rightSpeed = -.65;
+                } else {
+                    rightSpeed = .65;
+                }
+            }
+
+        } else if (participantGamepad.rightArrow.getValue()) {
+            if (!reversedTurns) {
+                if (rightSpeed == 0 && leftSpeed == 0) {
+                    rightSpeed = -.4;
+                    leftSpeed = .4;
+                } else if (rightSpeed > 0) {
+                    leftSpeed = .65;
+                } else {
+                    leftSpeed = -.65;
+                }
+            } else {
+                if (rightSpeed == 0 && leftSpeed == 0) {
+                    rightSpeed = .4;
+                    leftSpeed = -.4;
+                } else if (rightSpeed > 0) {
+                    leftSpeed = -.65;
+                } else {
+                    leftSpeed = .65;
+                }
+            }
+
+        }
+    }
+
+    public void armPositionUpdate() {
+
+        if (robot.currentArmState == ClawbotHardware.ArmState.MEDIUM && voltage > ClawbotHardware.MINIMUM_LOW_ARM_VOLTAGE) {
+            robot.arm.setPower(0.5);
+        } else if (robot.currentArmState == ClawbotHardware.ArmState.LOW && voltage < ClawbotHardware.MAXIMUM_MEDIUM_ARM_VOLTAGE) {
+            robot.arm.setPower(-0.2);
+        } else {
+            robot.arm.setPower(0);
+        }
+
+        if ((rightPower != 0 || leftPower != 0) && voltage > ClawbotHardware.MAXIMUM_LOW_ARM_VOLTAGE) {
+            robot.arm.setPower(0.3);
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Outreach/TeleOp/DDRPadDiagnostic.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode.Outreach.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Shared.Gamepad.DDRGamepad;
+
+@TeleOp(name = "DDR Pad Diagnostic", group = "Outreach")
+public class DDRPadDiagnostic extends OpMode {
+    static DDRGamepad participantGamepad;
+
+    @Override
+    public void init() {
+        participantGamepad = new DDRGamepad(this.gamepad1, new ElapsedTime(), "GP1");
+        telemetry.addLine("Do not Init program");
+        telemetry.addLine("Press Up Arrow");
+        telemetry.update();
+        if (participantGamepad.upArrow.isPressed()) {
+            telemetry.addLine("Up arrow works successfully");
+            telemetry.addLine("Press Left Arrow");
+            telemetry.update();
+        }
+        if (participantGamepad.leftArrow.isPressed()) {
+            telemetry.addLine("Left arrow works successfully");
+            telemetry.addLine("Press Right Arrow");
+            telemetry.update();
+        }
+        if (participantGamepad.rightArrow.isPressed()) {
+            telemetry.addLine("Right arrow works successfully");
+            telemetry.addLine("Press Down Arrow");
+            telemetry.update();
+        }
+        if (participantGamepad.downArrow.isPressed()) {
+            telemetry.addLine("Up arrow works successfully");
+            telemetry.addLine("Press top left Arrow");
+            telemetry.update();
+        }
+        if (participantGamepad.topLeftArrow.isPressed()) {
+            telemetry.addLine("top left Arrow successfully");
+            telemetry.addLine("Press top right Arrow");
+            telemetry.update();
+        }
+        if (participantGamepad.topRightArrow.isPressed()) {
+            telemetry.addLine("top right Arrow works successfully");
+            telemetry.addLine("Nothing is wrong with the hardware in the DDR Pad");
+            telemetry.update();
+        }
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addLine("Do not Init program");
+        telemetry.update();
+    }
+
+}
